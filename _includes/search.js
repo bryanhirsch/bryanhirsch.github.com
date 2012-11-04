6a4ddfdf884875b13d2f5c857d0c35111cd3822b
Bryan Hirsch <bryan@bryanhirsch.com>
2012-11-04 17:39:48 -0500

    bh-6 - Add Google search, using google.com/cse.
     	new file:   _includes/search.html
     	new file:   _includes/search.js

---
+++ _includes/search.js
@@ -0,0 +1,10 @@
+<!-- Put the following javascript before the closing </head> tag. -->
+<script>
+(function() {
+var cx = '001736848373915916864:teayqmyltd8';
+var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+'//www.google.com/cse/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
+</script>
